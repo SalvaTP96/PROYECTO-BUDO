e2b2bedfdeeb2594adec795c3c105974d240d1e4
SalvaTP <salvatp96@outlook.es>
2023-11-07 14:23:03 +0100

    first commit

---
+++ Cafeteria-Budo/Pruebas/script.js
@@ -0,0 +1,9 @@
+document.querySelector('.menu-toggle').addEventListener('click', function() {
+    const menu = document.querySelector('.menu');
+    menu.classList.toggle('active');
+});
+
+
+
+
+  
