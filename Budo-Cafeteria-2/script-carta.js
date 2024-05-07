a6dbcc386c0f624e72cea884fc459cc179c6558e
SalvaTP96 <salvatp96@outlook.es>
2024-05-07 18:45:04 +0200

    apartado de carta

---
+++ Budo-Cafeteria-2/script-carta.js
@@ -0,0 +1,20 @@
+document.getElementById('toggleCarta').addEventListener('click', function() {
+    var carta = document.getElementById('carta');
+    carta.classList.toggle('visible');
+});
+document.getElementById('toggleCarta2').addEventListener('click', function() {
+    var carta2 = document.getElementById('carta2');
+    carta2.classList.toggle('visible');
+});
+document.getElementById('toggleCarta3').addEventListener('click', function() {
+    var carta3 = document.getElementById('carta3');
+    carta3.classList.toggle('visible');
+});
+document.getElementById('toggleCarta4').addEventListener('click', function() {
+    var carta4 = document.getElementById('carta4');
+    carta4.classList.toggle('visible');
+});
+document.getElementById('toggleCarta5').addEventListener('click', function() {
+    var carta5 = document.getElementById('carta5');
+    carta5.classList.toggle('visible');
+});
